e863f949365eb9480892eeda481379fe59dd33e8
Ajsel Žilić <33603067+AveDominiInferni@users.noreply.github.com>
2022-10-25 14:12:21 +0200

    Delete ChannelInfo.js

--- frontend/src/components/ChannelInfo.js
+++
@@ -1,8 +0,0 @@
-export default function ChannelInfo({channelName, id, picture="", style}) {
-  return (
-    <div className="channel-info info-component" style={style}>
-      <div className="channel-pic info-pic-component"></div>
-      <div className="channel-name info-name-component">{channelName}</div>
-    </div>
-  );
-}
