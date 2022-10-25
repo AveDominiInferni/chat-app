0323b530936acc6bd94927fc7c2d690b51d9938a
Ajsel Žilić <33603067+AveDominiInferni@users.noreply.github.com>
2022-10-25 14:12:32 +0200

    Delete UserInfo.js

--- frontend/src/components/UserInfo.js
+++
@@ -1,8 +0,0 @@
-export default function UserInfo({username, id, picture="", handleClick}) {
-  return (
-    <div className="user-info info-component" onClick={() => handleClick(id)}>
-      <div className="user-pic info-pic-component"></div>
-      <div className="username info-name-component">{username}</div>
-    </div>
-  );
-}
